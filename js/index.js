2b9103660510002566cf54338c0a1039c9e21342
Enock <enochwanjala.97@gmail.com>
2023-10-25 09:37:58 +0300

    created and manipulated the card with DOM

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 //event listener for when our DOM loads
+let = 
 document.addEventListener("DOMContentLoaded", function(){
     getFilms()
 })
@@ -7,12 +8,16 @@ document.addEventListener("DOMContentLoaded", function(){
 function getFilms(){
     fetch("http://localhost:3000/films")
     .then(response =>response.json())
-    .then(films=>displayFilms(films))
+    .then(films=>{
+        displayFilmsTitle(films)
+        displayPosters(films)
+    })
 }
 
 //displaying film function
-function displayFilms(films){
-    for (const film of films ){
+function displayFilmsTitle(films){
+
+        for (const film of films ){
 
         //selecting elements
         const moviesList =document.querySelector('#movies-list');
@@ -26,7 +31,31 @@ function displayFilms(films){
 
         //appending 
         moviesList.append(list)
-        
-
     }
 }
+
+//display card details
+function displayPosters(films){
+    const filmsContainer = document.querySelector('#filmsCard')
+    for(const film of films){
+        filmsContainer.innerHTML+=` 
+        <div class="col-3 mx-1 p-2">
+        <div class="card" >
+            <img id="image" src="${film.poster}" alt=" movie poster">
+            <div class="card-body">
+              <h5 class="card-title">${film.title}</h5>
+              <p class="id">Id Number: ${film.id}</p>
+              <p class="runtime">Runtime:${film.runtime}</p>
+              <p class="showtime">Showtime: ${film.showtime}</p>
+              <p class="capacity"> Capacity: ${film.capacity}</p>
+              <p class="ticket_sold"> Ticket Sold: ${film.ticket_sold}</p>
+              <p class="card-text">${film.description}</p>
+              <a href="#" class="btn btn-primary">Buy Ticket</a>
+            </div>
+          </div>
+
+        `
+        filmsContainer.append(films)
+    }}
+
+
